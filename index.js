79dd01fe07a8e69012208da38aaa58fa5bc56514
brunoacev <brunoacev@gmail.com>
2026-05-31 17:23:38 -0300

    Feat: Workflow for create a new order

--- index.js
+++ index.js
@@ -27,12 +27,55 @@ while (opcao !== "0") {
 }
 
 function criarPedido() {
-  let cliente = prompt("Digite o nome do cliente: ");
+  let cliente = prompt("\nDigite o nome do cliente: ");
 
   let modelo = prompt(
-    "\n\nEscolha o modelo:\n\n" +
+    "\nEscolha o modelo:\n\n" +
       "1 - Cortina Tradicional\n" +
-      "2 - Cortina Motorizada\n" +
+      "2 - Cortina Motorizada\n\n" +
       "Digite a opção: ",
+  );
+
+  let nomeModelo = "";
+
+  if (modelo === "1") {
+    nomeModelo = "Cortina Tradicional";
+  } else if (modelo === "2") {
+    nomeModelo = "Cortina Motorizada";
+  } else {
+    console.log("Modelo inválido.");
+    return;
+  }
+
+  let quantidade = Number(prompt("Digite a quantidade: "));
+
+  if (quantidade <= 0) {
+    console.log("Quantidade inválida.");
+    return;
+  }
+
+  let pedido = [];
+  // [0] = ID -- [1] = Nome Cliente -- [2] = Modelo -- [3] = Quantidade do produto -- [4] = Status do pedido.
+
+  pedido[0] = pedidos.length + 1;
+  pedido[1] = cliente;
+  pedido[2] = nomeModelo;
+  pedido[3] = quantidade;
+  pedido[4] = "Pedido Criado";
+
+  console.log(
+    "\n\nPEDIDO CRIADO COM SUCESSO!\n\n" +
+      "ID: " +
+      pedido[0] +
+      "\n" +
+      "Cliente: " +
+      pedido[1] +
+      "\n" +
+      "Modelo: " +
+      pedido[2] +
+      "\n" +
+      "Quantidade: " +
+      pedido[3] +
+      "\n\n",
   );
 }
